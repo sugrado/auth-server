5f95c97c6da04c921c5a6c07b3b954c93dd59437
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-05-08 00:23:34 +0300

    auth api products controller

---
+++ AuthServer.API/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Entities.Concrete;
+using AuthServer.Core.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IServiceRepository<Product, ProductDto> _productService;
+
+        public ProductsController(IServiceRepository<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return ActionResultInstance(await _productService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto, productDto.Id));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] int id)
+        {
+            return ActionResultInstance(await _productService.Remove(id));
+        }
+    }
+}
